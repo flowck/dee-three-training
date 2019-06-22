f1349653991ccc7ce408754cf4f55c0d1ab00239
flowck <flowck96@gmail.com>
2019-06-22 15:43:09 +0100

    Render y and x grid

---
+++ 08-grid/grid.js
@@ -0,0 +1,62 @@
+// Sizes
+const margin = { t: 30, b: 20, l: 30, r: 20 };
+const width = 400 - margin.l - margin.r;
+const height = 200 - margin.t - margin.b;
+
+// Chart
+const svg = d3.select("svg")
+  .attr("width", width + margin.l + margin.r)
+  .attr("height", height + margin.t + margin.b)
+  .append("g")
+    .attr("transform", `translate(${margin.l}, ${margin.t})`);
+
+// Scales
+const xScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([0, width]);
+
+const yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0]);
+
+
+// Render grids
+const xGrid = () => {
+  return d3.axisBottom(xScale)
+    .ticks(5)
+    .tickSize(-height)
+    .tickFormat("");
+}
+
+const yGrid = () => {
+  return d3.axisLeft(yScale)
+    .ticks(5)
+    .tickSize(-width)
+    .tickFormat("");
+}
+
+// x grid
+svg.append("g")
+  .attr("class", "grid")
+  .attr("transform", `translate(0, ${height})`)
+  .call(xGrid());
+
+// y grid
+svg.append("g")
+  .attr("class", "grid")
+  .attr("transform", `translate(0, 0)`)
+  .call(yGrid());
+
+/**
+ * Render axis
+ */
+
+// x axis
+svg.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(d3.axisBottom(xScale));
+
+// y axis
+svg.append("g")
+  .attr("transform", `translate(0, 0)`)
+  .call(d3.axisLeft(yScale));
